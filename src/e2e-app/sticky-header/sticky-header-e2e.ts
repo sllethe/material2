b3f7444b5c92b023b36012d1e70e87245481c5c4
Lin Sun <2808411862@qq.com>
2017-06-19 15:40:01 -0700

    add e2e/sticky-header files

---
+++ src/e2e-app/sticky-header/sticky-header-e2e.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+    moduleId: module.id,
+    selector: 'sticky-header-e2e',
+    templateUrl: 'sticky-header-e2e.html',
+})
+export class StickyHeaderE2E { }
