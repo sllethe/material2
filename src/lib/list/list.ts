61e3666de3d69a9adf9870daedf9ea3bd73dbd4e
Lin Sun <2808411862@qq.com>
2017-07-12 15:53:30 -0700

    make md-list-option 'changeDetection: ChangeDetectionStrategy.OnPush', add chengeDetector

--- src/lib/list/list.ts
+++ src/lib/list/list.ts
@@ -7,27 +7,24 @@
  */
 
 import {
-  AfterContentInit, AfterViewInit,
+  AfterContentInit,
   Component,
   ContentChild,
   ContentChildren,
   Directive,
   ElementRef,
-  Input, OnInit,
+  Input,
   Optional,
   QueryList,
-  Renderer2, ViewChild, ViewChildren,
+  Renderer2,
   ViewEncapsulation,
   ChangeDetectionStrategy,
   OnDestroy,
   EventEmitter,
-  Output
+  Output, ChangeDetectorRef
 } from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {coerceBooleanProperty, MdLine, MdLineSetter, MdPseudoCheckbox, MdSelectionModule, SelectionModel} from '../core';
 import {FocusKeyManager} from '../core/a11y/focus-key-manager';
-import {MdCheckbox} from '../checkbox';
-import {MdCheckboxModule} from '../checkbox';
 import {Subscription} from 'rxjs/Subscription';
 import {SPACE, LEFT_ARROW, RIGHT_ARROW, TAB, HOME, END} from '../core/keyboard/keycodes';
 import {Focusable} from '../core/a11y/focus-key-manager';
@@ -90,7 +87,8 @@ export interface MdSelectionListOptionEvent {
     '[attr.aria-disabled]': 'disabled.toString()',
   },
   templateUrl: 'list-option.html',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MdListOption implements AfterContentInit, OnDestroy, Focusable {
   private _lineSetter: MdLineSetter;
@@ -145,6 +143,7 @@ export class MdListOption implements AfterContentInit, OnDestroy, Focusable {
 
   constructor(private _renderer: Renderer2,
               private _element: ElementRef,
+              private _changeDetector: ChangeDetectorRef,
               @Optional() public selectionList: MdSelectionList,) { }
 
 
@@ -169,22 +168,23 @@ export class MdListOption implements AfterContentInit, OnDestroy, Focusable {
 
     if(this._disabled == false) {
       this.selected = !this.selected;
+      this._changeDetector.markForCheck();
       this.selectionList.selectedOptions.toggle(this);
     }
     console.log(this.selectionList.selectedOptions);
-    console.log('current selectionModule: ' + this.selectionList.selectedOptions.selected.length);
+    console.log('current selectionModule length: ' + this.selectionList.selectedOptions.selected.length);
   }
 
-  onKeydown(e: KeyboardEvent): void {
-    // console.log('who onkeyDown: ' + this.pCheckbox);
-    if(e.keyCode === 32 && this._disabled == false) {
-      let focusedElement = document.activeElement;
-      console.log(focusedElement === this._element.nativeElement);
-      if(focusedElement === this._element.nativeElement) {
-        this.toggle();
-      }
-    }
-  }
+  // onKeydown(e: KeyboardEvent): void {
+  //   // console.log('who onkeyDown: ' + this.pCheckbox);
+  //   if(e.keyCode === 32 && this._disabled == false) {
+  //     let focusedElement = document.activeElement;
+  //     console.log(focusedElement === this._element.nativeElement);
+  //     if(focusedElement === this._element.nativeElement) {
+  //       this.toggle();
+  //     }
+  //   }
+  // }
 
   /** Allows for programmatic focusing of the option. */
   focus(): void {
