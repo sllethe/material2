50ab14c7a056cf8c9154e8ef56fba7f8e606ecd7
Lin Sun <2808411862@qq.com>
2017-07-26 15:08:05 -0700

    Added tests

--- src/lib/sticky-header/sticky-header.spec.ts
+++ src/lib/sticky-header/sticky-header.spec.ts
@@ -1,11 +1,11 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {Component, DebugElement} from '@angular/core';
-import {AnimationEvent} from '@angular/animations';
-import {StickyHeaderModule, CdkStickyRegion, CdkStickyHeader, STICKY_HEADER_SUPPORT_STRATEGY_PROVIDER} from './index';
-import {OverlayModule, Scrollable, OverlayContainer} from '../core/overlay/index';
+import {async, ComponentFixture, TestBed, fakeAsync, tick} from '@angular/core/testing';
+import {Component, DebugElement, ViewChild} from '@angular/core';
+import {StickyHeaderModule, CdkStickyRegion, CdkStickyHeader,
+  STICKY_HEADER_SUPPORT_STRATEGY_PROVIDER} from './index';
+import {OverlayModule, Scrollable} from '../core/overlay/index';
 import {PlatformModule} from '../core/platform/index';
-import {Platform} from '@angular/cdk/platform';
 import {By} from '@angular/platform-browser';
+import {dispatchFakeEvent} from '@angular/cdk/testing';
 
 
 
@@ -14,7 +14,7 @@ describe('sticky-header with positioning not supported', () => {
   let testComponent: StickyHeaderTest;
   let stickyElement: DebugElement;
   let stickyParentElement: DebugElement;
-  let scrollableElement: HTMLElement;
+  let scrollableElement: DebugElement;
   let stickyHeaderDir: CdkStickyHeader;
 
   beforeEach(async(() => {
@@ -34,18 +34,47 @@ describe('sticky-header with positioning not supported', () => {
     testComponent = fixture.debugElement.componentInstance;
     stickyElement = fixture.debugElement.query(By.directive(CdkStickyHeader));
     stickyParentElement = fixture.debugElement.query(By.directive(CdkStickyRegion));
-    // stickyHeaderDir = stickyElement.injector.get<CdkStickyHeader>(CdkStickyHeader);
-    // stickyHeaderDir = fixture.debugElement.query(By.directive(StickyHeaderDirective)).componentInstance;
-  });
-
-  fit('test test', () => {
-    expect(true).toBe(true);
+    stickyHeaderDir = stickyElement.injector.get<CdkStickyHeader>(CdkStickyHeader);
+    scrollableElement = fixture.debugElement.query(By.directive(Scrollable));
   });
 
   fit('should be able to find stickyParent when sticky positioning is not supported', () => {
     fixture.detectChanges();
     expect(stickyElement.nativeElement.stickyParent).not.toBe(null);
   });
+
+  fit('should stick when scrolled to the top of the container', fakeAsync(() => {
+    fixture.detectChanges();
+    let scrollableContainerTop = stickyHeaderDir.upperScrollableContainer.getBoundingClientRect().top;
+    expect(stickyHeaderDir.element.getBoundingClientRect().top).not.toBe(scrollableContainerTop);
+    tick(0);
+
+    // Scroll the page but tick just before the default throttle should update.
+    fixture.componentInstance.scrollDown();
+    tick(100);
+
+    expect(stickyHeaderDir.element.getBoundingClientRect().top).toBe(scrollableContainerTop);
+  }));
+
+  fit('should unstuck when scrolled off the top of the container', fakeAsync(() => {
+    fixture.detectChanges();
+    let scrollableContainerTop = stickyHeaderDir.upperScrollableContainer.getBoundingClientRect().top;
+    expect(stickyHeaderDir.element.getBoundingClientRect().top).not.toBe(scrollableContainerTop);
+    tick(0);
+
+    // Scroll the page but tick just before the default throttle should update.
+    fixture.componentInstance.scrollDown();
+    tick(100);
+
+    expect(stickyHeaderDir.element.getBoundingClientRect().top).toBe(scrollableContainerTop);
+
+    // Scroll the page but tick just before the default throttle should update.
+    fixture.componentInstance.scrollBack();
+    tick(100);
+
+    expect(stickyHeaderDir.element.getBoundingClientRect().top).not.toBe(scrollableContainerTop);
+
+  }));
 
   function mockStickyHeaderCheckSuccess() {
     return true;
@@ -149,6 +178,23 @@ describe('sticky-header with positioning supported', () => {
     </div>
   `})
 class StickyHeaderTest {
+  @ViewChild(Scrollable) scrollingContainer: Scrollable;
+
+  scrollDown() {
+    const scrollingContainerEl = this.scrollingContainer.getElementRef().nativeElement;
+    scrollingContainerEl.scrollTop = 300;
+
+    // Emit a scroll event from the scrolling element in our component.
+    dispatchFakeEvent(scrollingContainerEl, 'scroll');
+  }
+
+  scrollBack() {
+    const scrollingContainerEl = this.scrollingContainer.getElementRef().nativeElement;
+    scrollingContainerEl.scrollTop = 0;
+
+    // Emit a scroll event from the scrolling element in our component.
+    dispatchFakeEvent(scrollingContainerEl, 'scroll');
+  }
 }
 
 
